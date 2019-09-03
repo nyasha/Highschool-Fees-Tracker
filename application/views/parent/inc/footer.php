30c72656a0bc018b3f1ce34751c84668afcf3dac
Denis John <denis.john.ndarake@gmail.com>
2019-09-03 10:52:30 +0100

    Updated parent route, so each parent can only see his/her ward related payments

---
+++ application/views/parent/inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      
+    </div>
+    <!-- Default to the left -->
+    <strong>Crafted with&nbsp;<i class="fa fa-heart"></i>&nbsp;by&nbsp;<a target="_blank" href="">Choras</a></strong>
+  </footer>
