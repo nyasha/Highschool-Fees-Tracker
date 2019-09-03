a922bca27434376cccd553b1ba3fcf99c15f4d08
Denis John <denis.john.ndarake@gmail.com>
2019-09-03 11:37:39 +0100

    Updated admin dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -35,9 +35,93 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="content">
       <div class="container-fluid">
         <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>
+                  <?php  
+                  $this->db->from('student');
+                  $this->db->where('SESSION', $current_session);
+                  $numquery = $this->db->get();
+                  echo $numquery->num_rows();
+                  ?>
+                </h3>
 
+                <p>TOTAL STUDENTS</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <a href="#" class="small-box-footer">&nbsp;</a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>
+                  <?php  
+                  $this->db->from('parent_tbl');
+                  $numquery = $this->db->get();
+                  echo $numquery->num_rows();
+                  ?>
+                </h3>
+
+                <p>TOTAL PARENTS</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <a href="#" class="small-box-footer">&nbsp;</a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>
+                  <?php  
+                  $this->db->from('users_tbl');
+                  $numquery = $this->db->get();
+                  echo $numquery->num_rows();
+                  ?>
+                </h3>
+
+                <p>TOTAL USERS</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+              <a href="#" class="small-box-footer">&nbsp;</a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>
+                  <?php  
+                  $this->db->select('SUM(AMOUNT_PAID) as amt');
+                  $this->db->where('SESSION', $current_session);
+                  $q=$this->db->get('all_payments_tbl');
+                  $r=$q->row();
+                  echo '&#8358;'.number_format($r->amt);
+                  ?>
+                </h3>
+                <p>TOTAL PAYMENT</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-money"></i>
+              </div>
+              <a href="#" class="small-box-footer">&nbsp;</a>
+            </div>
+          </div>
+          <!-- ./col -->
         </div>
-        <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
